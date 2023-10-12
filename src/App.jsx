1d5e0bbd8b1958c000927906e55de5a2bc22ebda
davidBilson <davidbilson99@gmail.com>
2023-10-12 14:43:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <section className="birthday-message">
+      <h1 className="message-title">From David Bilson to Stephen Vizzusi</h1>
+
+      <p className="message-paragraph">Dear Stephen,</p>
+
+      <p className="message-paragraph">As you celebrate your birthday today, October 12, 2023, I want to take this moment to not just celebrate your birthday but to appreciate you for the incredible person you are.</p>
+
+      <p className="message-paragraph">I remember meeting you on discord, and I never thought you'd turn out to be someone I look up to in terms of discipline, consistency, and personal values.</p>
+
+      <p className="message-paragraph">As you add another year to your journey today, I hope it's filled with boundless joy, a new GIRLFRIEND, haha, lol, a new software engineering job at any company you like, and so many other fulfilling new journeys. You deserve every bit of happiness and kindness in the world.</p>
+
+      <p className="message-paragraph">I also want to appreciate you for everything you've done for me so far! I will forever be grateful for it all!!!!</p>
+
+      <p className="message-paragraph">Happy birthday to you, Stephen!!! CHEERS TO MORE LIFE!</p>
+
+      <p className="message-sender">With gratitude and love,</p>
+      <p className="message-sender">David Bilson</p>
+    </section>
+  );
+};
+
+export default App;
